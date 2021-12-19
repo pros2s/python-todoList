a85a0ed508b3b8f76886c4d847cf64cf6f7e5809
Alexandr <caiiiia_podoplelov@mail.ru>
2021-12-20 01:34:30 +0300

    added bootstrap and some styles

--- taskManage/taskManage/settings.py
+++ taskManage/taskManage/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'mainWeb'
 ]
 
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
