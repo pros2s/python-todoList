cc76ef5d701b3db6d5291e58139c8d1a267d586f
Alexandr <caiiiia_podoplelov@mail.ru>
2021-12-19 22:43:38 +0300

    Created django project

---
+++ taskManage/mainWeb/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, '')
+
+
+def about(request):
+    return HttpResponse('<h4>About</h4>')
