a9e6ca629fe14a9d96d60f5ff096528a8cf0da35
Alexandr <caiiiia_podoplelov@mail.ru>
2021-12-20 04:38:58 +0300

    post request to database and output tasks

--- taskManage/mainWeb/views.py
+++ taskManage/mainWeb/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    return render(request, 'mainWeb/index.html')
+    tasks = Task.objects.order_by('-id')[:4]
+    return render(request, 'mainWeb/index.html', {
+        'title': 'Главная страница',
+        'tasks': tasks
+    })
 
 
 def about(request):
-    return render(request, 'mainWeb/about.html')
+    return render(request, 'mainWeb/about.html', {
+        'title': 'О нас'
+    })
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Неверные данные'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'mainWeb/create.html', context)
